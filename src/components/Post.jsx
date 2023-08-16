d0e8f095f18b9bb3a9ffd4e81fd6177e46a83238
Pablo del Barrio <pablodbb@gmail.com>
2023-08-16 20:05:43 +0200

    fixed position of dropdown in mobile

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -158,7 +158,7 @@ export default function Post({ posts }) {
                 <MoreButton />
               </div>
               {openDropdownIndex === index && (
-                <div className="absolute ml-[0px] mt-[5px] bg-white border border-gray-300 shadow-lg">
+                <div className="absolute ml-[-131px] mt-[5px] md:ml-[0px] md:mt-[5px] bg-white border border-gray-300 shadow-lg">
                   <div className="flex hover:bg-fx-gray pt-3 px-2 cursor-pointer">
                     <HideIcon />
                     <span className="text-fx-text-post ml-3 mb-2">Hide</span>
